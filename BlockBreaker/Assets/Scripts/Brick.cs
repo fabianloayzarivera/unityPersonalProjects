1ad284b949547faee611c1d0e8c9499a636afcb0
Floayza <fabianloayzarivera@gmail.com>
2017-09-09 22:51:52 -0400

    First commit

---
+++ BlockBreaker/Assets/Scripts/Brick.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+	public static int breakableCount = 0;
+
+	private int maxHits;
+	private int timesHit;
+	public Sprite[] hitSprites;
+	private LevelManager levelmanager;
+	private bool isBreakable;
+	// Use this for initialization
+	void Start () {
+		isBreakable = (this.tag == "Breakable");
+		if (isBreakable) {
+			breakableCount++;
+		}
+		timesHit = 0;
+		levelmanager = GameObject.FindObjectOfType<LevelManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D col){
+		
+		if (isBreakable) {
+			handleHits ();
+		}
+	
+	}
+
+	void handleHits(){
+		timesHit++;
+		maxHits = hitSprites.Length + 1;
+		//simulateWin ();
+		if (timesHit >= maxHits) {
+			breakableCount--;
+			levelmanager.brickDestroyed ();
+			Destroy (gameObject);
+			//print (breakableCount);
+		} else {
+			LoadSprites ();
+		}
+	}
+
+	void simulateWin(){
+		levelmanager.LoadNextLevel ();
+	}
+
+	void LoadSprites(){
+		int spriteIndex = timesHit - 1;
+		if (hitSprites [spriteIndex]) {
+			this.GetComponent<SpriteRenderer> ().sprite = hitSprites [spriteIndex];
+		}
+	}
+}
---
+++ BlockBreaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance =null;
+	// Use this for initialization
+
+	void Awake(){
+		Debug.Log ("Music Player Awake " + GetInstanceID());
+		if (instance != null){
+			Destroy(gameObject);
+			print("Duplicate Music Player self-destructed");
+		}else{
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);	
+		}
+	}
+	void Start () {
+		Debug.Log ("Music Player Start " + GetInstanceID());
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
